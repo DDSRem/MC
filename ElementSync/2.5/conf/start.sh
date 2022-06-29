a5ea10cb406df5a939e546bb7d871f6f84460546
DDSRem <1448139087@qq.com>
2022-06-29 10:20:33 +0800

    elementsync

--- ElementSync/2.5/conf/start.sh
+++ ElementSync/2.5/conf/start.sh
@@ -1,13 +1,13 @@
 #!/bin/sh
 
-if [ ! -f "/ElementSync/ElementSync.jar" ]; then
+if [ ! -f "/app/ElementSync.jar" ]; then
  echo -e "\033[36m ElementSync.jar文件不存在,正在创建... \033[0m"
- mv /opt/ElementSync.jar /ElementSync
+ mv /opt/ElementSync.jar /app
  echo "ElementSync.jar文件创建成功"
 else
  echo "ElementSync.jar文件存在"
  rm -rf /opt/ElementSync.jar
 fi
 
-cd /ElementSync
+cd /app
 exec java -jar -Dfile.encoding=UTF-8 -Xmx64m ElementSync.jar
